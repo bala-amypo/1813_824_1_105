f97fe966ca8c734feca1415233934cc8ad9c1c03
bala-amypo <bala@amypo.in>
2025-12-17 10:44:08 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,8 +1,8 @@
 package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.peresistence.GenerationType;
-import jakarta.peresistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 @Entity
 public class Visitor{
