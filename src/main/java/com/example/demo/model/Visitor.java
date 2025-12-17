628a33f8863007b5f043076fbd631e8397af3385
bala-amypo <bala@amypo.in>
2025-12-17 10:34:01 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,70 +1,70 @@
-// package com.example.demo.model;
-// import jakarta.presistence.Entity;
-// import jakarta.presistence.GeneratedValue;
-// import jakarta.presistence.GenerationType;
-// import jakarta.presistence.Id;
-// import jakarta.presistence.constraints.NotNull;
-// @Entity
-// public class Visitor{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     @NotNull("This field is required")
-//     private String fullName;
-//     private String email;
-//     private String phone;
-//     @NotNull("This field is required")
-//     private String idProofNumber;
-//     private String createdAt;
+package com.example.demo.model;
+import jakarta.presistence.Entity;
+import jakarta.presistence.GeneratedValue;
+import jakarta.presistence.GenerationType;
+import jakarta.presistence.Id;
+import jakarta.presistence.constraints.NotNull;
+@Entity
+public class Visitor{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @NotNull("This field is required")
+    private String fullName;
+    private String email;
+    private String phone;
+    @NotNull("This field is required")
+    private String idProofNumber;
+    private String createdAt;
     
-//     public Long  getId(){
-//         return id;
-//     }
-//     public void setId(int id){
-//         this.id=id;
-//     }
-//     public String getFullName(){
-//         return fullName;
-//     }
-//     public void setFullName(string fullName){
-//         this.fullName=fullName;
-//     }
-//     public String getEmail(){
-//         return email;
-//     }
-//     public void setEmail(String email){
-//         this.email=email;
-//     }
-//     public String getPhone(){
-//         return phone;
-//     }
-//     public void setPhone(String phone){
-//         this.phone=phone;
-//     }
-//     public String getIdProofNumber(){
-//         return idProofNumber;
-//     }
-//     public void setIdproofNumber(String idProofNumber){
-//         this.idProofNumber=idProofNumber;
-//     }
-//     public String getCreatedAt(){
-//         return createdAt;
-//     }
-//     public void setCreatedAt(String createdAt){
-//         this.createdAt=createdAt;
-//     }
-//     public Visitor(Long id,String fullname,String email,String phone,String idProofNumber,String LocalDateTime){
-//         this.id=id;
-//         this.fullName=fullName;
-//         this.email=email;
-//         this.phone=phone;
-//         this.idProofNumber=idProofNumber;
-//         this.createdAt=createdAt;
-//     }
-//     public Visitor(){
+    public Long  getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(string fullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+    public String getIdProofNumber(){
+        return idProofNumber;
+    }
+    public void setIdproofNumber(String idProofNumber){
+        this.idProofNumber=idProofNumber;
+    }
+    public String getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt){
+        this.createdAt=createdAt;
+    }
+    public Visitor(Long id,String fullname,String email,String phone,String idProofNumber,String LocalDateTime){
+        this.id=id;
+        this.fullName=fullName;
+        this.email=email;
+        this.phone=phone;
+        this.idProofNumber=idProofNumber;
+        this.createdAt=createdAt;
+    }
+    public Visitor(){
 
-//     }
-//     Visitor obj1=new Visitor();
+    }
+    
     
 
-// }
+}
