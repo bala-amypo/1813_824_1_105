c836bb4935d3e3cf319ebd4b0366cb697c656c2f
bala-amypo <bala@amypo.in>
2025-12-18 04:05:14 +0000

    User commit

--- src/main/java/com/example/demo/model/Appointment.java
+++ src/main/java/com/example/demo/model/Appointment.java
@@ -1,2 +1,12 @@
 package com.example.demo.model;
-class
+public class Appointment{
+    @Id
+    private Long id;
+    private String visitor;
+    private String host;
+    private String appointmentDate;
+    private String purpose;
+    private String status;
+    
+
+}
