05731b9f399c769da053d23a0de26cafe2fec568
bala-amypo <bala@amypo.in>
2025-12-18 10:32:43 +0000

    User commit

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -6,5 +6,5 @@ public interface AppointmentService{
     public Appointment createAppoinment(Long visitorId,Appointment appointment);
     public Appointment getAppointment(Long id);
     public Appointment getAppointmentsForHost(Long hostId);
-    public Appointment getAppointmentsForVisitor 
+    public Appointment getAppointmentsForVisitor(Long visitorId); 
 }
