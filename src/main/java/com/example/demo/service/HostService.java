67450f001b002f46f7112b2adfdf13e13d6a7b92
bala-amypo <bala@amypo.in>
2025-12-18 10:27:26 +0000

    User commit

--- src/main/java/com/example/demo/service/HostService.java
+++ src/main/java/com/example/demo/service/HostService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 import jakarta.persistence.service;
 import com.example.demo.model.Host;
+@Service
 public interface HostService{
-    public 
+    public Host createHost(Host host);
+    public Host getHost(Long id);
+    public Host getAllHosts();
 }
