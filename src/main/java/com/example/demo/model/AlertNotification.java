685c41e05372ba9911d2d973f90a58e962dc6dc6
bala-amypo <bala@amypo.in>
2025-12-18 07:57:27 +0000

    User commit

--- src/main/java/com/example/demo/model/AlertNotification.java
+++ src/main/java/com/example/demo/model/AlertNotification.java
@@ -6,11 +6,33 @@ import jakarta.persistence.GenerationType;
 @Entity
 public class AlertNotification{
     @Id
+    @GeneratedValue(strategy.GenerationType.IDENTITY)
     private Long id;
     private String visitLog;
     private String sentTo;
     private String alertMessage;
     private LocalDateTime sentAt;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getVisitLog(){
+        return visitLog;
+    }
+    public voic setVisitLog(String visitLog){
+        this.visitLog=visitLog;
+    }
+    public String getSentTo(){
+        return sentTo;
+    }
+    public voic setSentTo(String visitLog){
+        this.visitLog=visitLog;
+    }
+    
+    
 
 
 
