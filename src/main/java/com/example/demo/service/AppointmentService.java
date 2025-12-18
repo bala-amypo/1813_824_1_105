87f7c71b38d0e85d9b4530753670ada1ebe05187
bala-amypo <bala@amypo.in>
2025-12-18 10:32:26 +0000

    User commit

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,3 +1,10 @@
 package com.example.demo.service;
 import jakarta.persistence.Service;
-import com.example.demo.model
+import com.example.demo.model.Appointment;
+@Service
+public interface AppointmentService{
+    public Appointment createAppoinment(Long visitorId,Appointment appointment);
+    public Appointment getAppointment(Long id);
+    public Appointment getAppointmentsForHost(Long hostId);
+    public Appointment getAppointmentsForVisitor 
+}
