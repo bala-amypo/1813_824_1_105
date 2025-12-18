b364014b562d26805667bdb4cc2a772112bd08c1
bala-amypo <bala@amypo.in>
2025-12-18 09:17:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertNotificationrepository.java
+++ src/main/java/com/example/demo/repository/AlertNotificationrepository.java
@@ -1,2 +1,6 @@
 package com.example.demo.repository;
-import org.springframework.data
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.AlterNotification;
+public interface AlterNotificationRepository extends JpaRepository<AlterNotification,Long>{
+
+}
