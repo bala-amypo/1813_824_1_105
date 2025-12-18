90382943fbc0f4c189f9c0f2723521202ee7a3c7
bala-amypo <bala@amypo.in>
2025-12-18 16:23:06 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertNotificationService.java
+++ src/main/java/com/example/demo/service/AlertNotificationService.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Service;
 import com.example.demo.model.AlertNotification;
 @Service
 public interface AlertNotificationService{
-    public AlertNotification sendAlert(Long visitLogId)
-    public AlertNotification 
+    public AlertNotification sendAlert(Long visitLogId);
+    public AlertNotification getAlert(Long id);
+    public AlertNotification getAllAlert();
 }
