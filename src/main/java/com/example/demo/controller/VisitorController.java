f008a7541a1ddf89cd47344f47728d8b47222dfd
bala-amypo <bala@amypo.in>
2025-12-18 17:18:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -5,4 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.entity.Visitor;
+import org.springframework.web.bind.annotation.Autowired;
+import com.example.demo.service.VisitorService;
+@RestController
+public class VisitorController{
+    @Autowired
+    VisitorService obj;
+}
