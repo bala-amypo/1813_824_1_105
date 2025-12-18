d0355c9f96c8ea1cfc22ae8789778d568d8c464b
bala-amypo <bala@amypo.in>
2025-12-18 10:12:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/VisitorService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+import jakarta.persistence.service;
+import com.example.demo.model.Visitor;
+@Service
+public interface VisitorService{
+  public Visitor createVisitor(Visitor visitor);
+
+ public Visitor getVisitor(Long id);
+ public 
+}
