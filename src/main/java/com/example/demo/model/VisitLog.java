d6c41bc9c4d419ee785d1b7ee48e5d52793c3119
bala-amypo <bala@amypo.in>
2025-12-18 05:10:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+public class VisitLog{
+    private Long id;
+    private String visitor;
+    private String host;
+    private String checkInTime;
+    private String checkOutTime;
+    private String
+
+}
