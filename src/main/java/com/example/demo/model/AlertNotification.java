26020bb95f2cff15b2b0c18311f947e60d8ade7a
bala-amypo <bala@amypo.in>
2025-12-18 06:53:31 +0000

    User commit

--- src/main/java/com/example/demo/model/AlertNotification.java
+++ src/main/java/com/example/demo/model/AlertNotification.java
@@ -2,6 +2,11 @@ package com.example.demo.model;
 @Entity
 public class AlertNotification{
     @Id
-    
+    private Long id;
+    private String visitLog;
+    private String sentTo;
+    private String alertMessage;
+    private LocalDateTime sentAt;
+
 
 }
