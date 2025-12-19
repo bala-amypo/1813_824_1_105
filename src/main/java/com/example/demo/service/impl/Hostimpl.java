79e7c06b98b7846e4c39d44226706ffcbe78b868
bala-amypo <bala@amypo.in>
2025-12-19 08:42:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Hostimpl.java
+++ src/main/java/com/example/demo/service/impl/Hostimpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 import com.example.demo.repository.Hostrepository;
-import com.example.demo.controller.Hostcontroller;
+import com.example.demo.service.HostService;
 import com.example.demo.model.Host;
 import java.util.List;
 import org.springframework.stereotype.Service;
