66901f0abb9237965e8bf7b166997d8cafe6bb0d
bala-amypo <bala@amypo.in>
2025-12-19 09:42:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Appointmentimpl.java
+++ src/main/java/com/example/demo/service/impl/Appointmentimpl.java
@@ -2,4 +2,5 @@ package com.example.demo.service.impl;
 import com.example.demo.controller.AppointmentController;
 import com.example.demo.model.Appointment;
 import org.springframework.stereotype.Service;
-import com.example.demo.
+import com.example.demo.service.AppointmentService;
+public 
