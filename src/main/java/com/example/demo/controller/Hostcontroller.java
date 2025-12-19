6fd90a52bce233644eb355dd937305a343aa183d
bala-amypo <bala@amypo.in>
2025-12-19 08:06:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/Hostcontroller.java
+++ src/main/java/com/example/demo/controller/Hostcontroller.java
@@ -1,3 +1,10 @@
 package com.example.demo.controller;
 import com.example.demo.service.HostService;
-import org.springframework.
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.
