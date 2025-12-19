dfb4bb92fcd5cc5ab8618fb396a1886b4021263c
bala-amypo <bala@amypo.in>
2025-12-19 10:09:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Appointmentimpl.java
+++ src/main/java/com/example/demo/service/impl/Appointmentimpl.java
@@ -1,13 +1,13 @@
-package com.example.demo.service.impl;
-import com.example.demo.controller.AppointmentController;
-import com.example.demo.model.Appointment;
-import org.springframework.stereotype.Service;
-import com.example.demo.service.AppointmentService;
-@Service
-public class Appointmentimpl implements AppointmentService{
-    @Autowired
-    @Appointmentrepository obj;
-    public Appointment createAppointment(Long visitorId,Long hostId,Appointment appointment){
-        return obj
-    }
-}
+// package com.example.demo.service.impl;
+// import com.example.demo.controller.AppointmentController;
+// import com.example.demo.model.Appointment;
+// import org.springframework.stereotype.Service;
+// import com.example.demo.service.AppointmentService;
+// @Service
+// public class Appointmentimpl implements AppointmentService{
+//     @Autowired
+//     @Appointmentrepository obj;
+//     public Appointment createAppointment(Long visitorId,Long hostId,Appointment appointment){
+//         return obj
+//     }
+// }
