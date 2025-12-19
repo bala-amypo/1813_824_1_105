21150c609bd967beb3c52651ab01d7153f0aedd1
bala-amypo <bala@amypo.in>
2025-12-19 08:00:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Hostimpl.java
+++ src/main/java/com/example/demo/service/impl/Hostimpl.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 public class  Hostimpl extends HostService{
     @Autowired
-    
+    public Host createHost(Host host){
+        
+    }
 }
