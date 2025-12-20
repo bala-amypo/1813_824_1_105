67f5105bfca20276a6b1db13f6720d3a8acf5bc1
bala-amypo <bala@amypo.in>
2025-12-20 13:29:08 +0000

    User commit

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 import com.example.demo.model.Appointment;
 public interface AppointmentService{
-    Appointment createAppoinment(Long visitorId,Long hostId,Appointment appointment);
+    Appointment createAppointment(Long visitorId,Long hostId,Appointment appointment);
     Appointment getAppointment(Long id);
     List<Appointment> getAppointmentsForHost(Long hostId);
     List<Appointment> getAppointmentsForVisitor(Long visitorId); 
