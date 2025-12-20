2a16d78357ecc1eea683dbac3e6d0d9a97a4b2e5
bala-amypo <bala@amypo.in>
2025-12-20 16:59:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/Appointmentrepository.java
+++ src/main/java/com/example/demo/repository/Appointmentrepository.java
@@ -3,4 +3,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Appointment;
 public interface Appointmentrepository extends JpaRepository<Appointment,Long>{
 
+    List<Appointment> findByHost(String host);
+
+    List<Appointment> findByVisitor(String visitor);
+
 }
