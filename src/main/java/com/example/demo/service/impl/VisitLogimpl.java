d63e890d612a8de22c9c7ee20406fd67a43d45b8
bala-amypo <bala@amypo.in>
2025-12-21 02:21:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitLogimpl.java
+++ src/main/java/com/example/demo/service/impl/VisitLogimpl.java
@@ -9,7 +9,15 @@ public class VisitLogimpl implements VisitLogService{
     VisitLogrepository obj;
     public VisitorLog checkInVisitor(Long visitorId,Long hostId,String purpose){
           VisitLog log=new Visitor();
-          log.setVisitor(visitorId.)
+          log.setVisitor(visitorId.toString());
+          log.setHost(hostId.toString());
+          log.setPurpose(purpose);
+          log.setAccessGranted(true);
+          return obj.save(log;)
+    }
+    public VisitLog checkOutVisitor(Long visitLogId){
+        Visitor log=obj.findById(VisitLogId).orElse(null);
+        log.setCheckOutTime(Local)
     }
 
 }
