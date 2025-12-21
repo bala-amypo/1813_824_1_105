2a784498e11b0d1a5a5d6cc096351b47ceefc718
bala-amypo <bala@amypo.in>
2025-12-21 02:16:45 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitLogService.java
+++ src/main/java/com/example/demo/service/VisitLogService.java
@@ -1,10 +1,11 @@
 package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.VisitLog;
+import java.util.List;
 
 public interface VisitLogService{
-    public VisitLog checkInVisitor(Long visitorId,Long hostId,String purpose);
-    public VisitLog checkOutVisitor(Long visitLogId);
-    public VisitLog getActiveVisits();
-    public VisitLog getVisitLog(Long id);
+  VisitLog checkInVisitor(Long visitorId,Long hostId,String purpose);
+   VisitLog checkOutVisitor(Long visitLogId);
+ List<VisitLog> getActiveVisits();
+ List<VisitLog> getVisitLog(Long id);
 }
