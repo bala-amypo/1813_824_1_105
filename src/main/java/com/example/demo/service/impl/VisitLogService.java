fecb88876953cce43809dd8404211780e962803d
bala-amypo <bala@amypo.in>
2025-12-21 01:57:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitLogService.java
+++ src/main/java/com/example/demo/service/impl/VisitLogService.java
@@ -1,3 +1,11 @@
 package com.example.demo.impl;
 import com.example.demo.repository.VisitLogrepository;
 import com.example.demo.service.VisitLogService;
+import org.springframework.streotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+@Service
+public interface VisitLogrepositoryService{
+    @Autowired
+    Vis
+
+}
