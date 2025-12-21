f71c790c58da4cc7bca13c04eea99de8932e7f80
bala-amypo <bala@amypo.in>
2025-12-21 02:15:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitLogrepository.java
+++ src/main/java/com/example/demo/repository/VisitLogrepository.java
@@ -1,6 +1,7 @@
-// package com.example.demo.repository;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.model.VisitLog;
-// public interface VisitLogrepository extends JpaRepository<VisitLog,Long>{
-
-// }
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.VisitLog;
+import java.util.List;
+public interface VisitLogrepository extends JpaRepository<VisitLog,Long>{
+  List<visitLog> findBycheckOutTimeIsNull();
+}
--- src/main/java/com/example/demo/service/VisitLogService.java
+++ src/main/java/com/example/demo/service/VisitLogService.java
@@ -1,10 +1,10 @@
-// package com.example.demo.service;
-// import jakarta.persistence.service;
-// import com.example.demo.model.VisitLog;
-// @Service
-// public interface VisitLogService{
-//     public VisitLog checkInVisitor(Long visitorId,Long hostId,String purpose);
-//     public VisitLog checkOutVisitor(Long visitLogId);
-//     public VisitLog getActiveVisits();
-//     public VisitLog getVisitLog(Long id);
-// }
+package com.example.demo.service;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.VisitLog;
+
+public interface VisitLogService{
+    public VisitLog checkInVisitor(Long visitorId,Long hostId,String purpose);
+    public VisitLog checkOutVisitor(Long visitLogId);
+    public VisitLog getActiveVisits();
+    public VisitLog getVisitLog(Long id);
+}
