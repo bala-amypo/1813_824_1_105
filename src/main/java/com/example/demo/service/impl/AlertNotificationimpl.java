48d63a8b7e91bf5500522a8ae5f943b00d9865ea
bala-amypo <bala@amypo.in>
2025-12-22 13:38:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertNotificationimpl.java
+++ src/main/java/com/example/demo/service/impl/AlertNotificationimpl.java
@@ -10,20 +10,29 @@ import com.example.demo.repository.AlertNotificationrepository;
 import com.example.demo.service.AlertNotificationService;
 @Service
 public class AlertNotificationimpl implements AlertNotificationService {
+
     @Autowired
-    AlertNotificationrepository obj;
+    AlertNotificationRepository obj;
+
+    @Override
     public AlertNotification sendAlert(Long visitLogId) {
+
         AlertNotification alert = new AlertNotification();
-        alert.setVisitLogId(visitLogId);
-        alert.setMessage("New alert");
-        alert.setCreatedAt(LocalDateTime.now());
+
+        alert.setVisitLog(String.valueOf(visitLogId)); // convert Long → String
+        alert.setAlertMessage("New alert");
+        alert.setSentAt(LocalDateTime.now());
+
         return obj.save(alert);
     }
 
+    @Override
     public AlertNotification getAlert(Long id) {
         return obj.findById(id)
                 .orElseThrow(() -> new RuntimeException("Alert not found"));
     }
+
+    @Override
     public List<AlertNotification> getAllAlert() {
         return obj.findAll();
     }
