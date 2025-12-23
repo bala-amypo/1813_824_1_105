10b53c979de8a5299365a0db58ba81ed3a9d028d
bala-amypo <bala@amypo.in>
2025-12-23 04:18:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertNotificationController.java
+++ src/main/java/com/example/demo/controller/AlertNotificationController.java
@@ -1,23 +1,29 @@
 package com.example.demo.controller;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.example.demo.model.AlertNotification;
 import com.example.demo.service.AlertNotificationService;
+
 @RestController
-@RequestMapping("alerts")
+@RequestMapping("/alerts")
 public class AlertNotificationController {
+
     @Autowired
     private AlertNotificationService service;
-    @PostMapping
-    public AlertNotification sendAlert(
-            @RequestBody AlertNotification alert) {
-        return service.sendAlert(alert);
+
+    @PostMapping("/{visitLogId}")
+    public AlertNotification sendAlert(@PathVariable Long visitLogId) {
+        return service.sendAlert(visitLogId);
     }
     @GetMapping("/{id}")
     public AlertNotification getAlert(@PathVariable Long id) {
         return service.getAlert(id);
     }
+
     @GetMapping
     public List<AlertNotification> getAllAlerts() {
         return service.getAllAlerts();
