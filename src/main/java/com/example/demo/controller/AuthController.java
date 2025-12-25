28c13240880364bd8fb6299777fc0c083ac728ac
bala-amypo <bala@amypo.in>
2025-12-25 14:32:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,18 +1,18 @@
-package com.example.demo.controller;
-import com.example.demo.service.UserService;
-import com.example.demo.model.User;
-import org.springframework.web.bind.annotation.RequestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-@RequestController
-@RequestMapping("/auth")
-public class AuthController{
-   @Autowired
-   UserService obj;
-   public User hello()
-}
+// package com.example.demo.controller;
+// import com.example.demo.service.UserService;
+// import com.example.demo.model.User;
+// import org.springframework.web.bind.annotation.RequestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+// @RequestController
+// @RequestMapping("/auth")
+// public class AuthController{
+//    @Autowired
+//    UserService obj;
+//    public User hello()
+// }
