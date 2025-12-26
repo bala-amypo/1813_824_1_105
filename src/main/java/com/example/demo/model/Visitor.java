b79f50542e3b2a844722405e2e2ab9ca06a2dfe0
bala-amypo <bala@amypo.in>
2025-12-26 06:45:40 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,69 +1,24 @@
 package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-@Entity
-public class Visitor{
-    @Id
+
+public class Visitor {
     private Long id;
-    @NotNull(message="This field is required")
     private String fullName;
-    private String email;
     private String phone;
-    @NotNull(message="This field is required")
     private String idProofNumber;
-    private String createdAt;
-    
-    public Long  getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getFullName(){
-        return fullName;
-    }
-    public void setFullName(String fullName){
-        this.fullName=fullName;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getPhone(){
-        return phone;
-    }
-    public void setPhone(String phone){
-        this.phone=phone;
-    }
-    public String getIdProofNumber(){
-        return idProofNumber;
-    }
-    public void setIdProofNumber(String idProofNumber){
-        this.idProofNumber=idProofNumber;
-    }
-    public String getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(String createdAt){
-        this.createdAt=createdAt;
-    }
-    public Visitor(Long id,String fullname,String email,String phone,String idProofNumber,String LocalDateTime){
-        this.id=id;
-        this.fullName=fullName;
-        this.email=email;
-        this.phone=phone;
-        this.idProofNumber=idProofNumber;
-        this.createdAt=createdAt;
-    }
-    public Visitor(){
+    private String email;
 
-    }
-    
-    
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getIdProofNumber() { return idProofNumber; }
+    public void setIdProofNumber(String idProofNumber) { this.idProofNumber = idProofNumber; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
