36f403325f36e34e55039ef17983c4ae84bc0eb8
bala-amypo <bala@amypo.in>
2025-12-26 09:52:51 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,10 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Visitor {
@@ -16,8 +12,6 @@ public class Visitor {
     private String fullName;
     private String phone;
     private String email;
-
-    @Column(unique = true)
     private String idProofNumber;
 
     public Long getId() { return id; }
