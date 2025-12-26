ec7384cbddf8d58f755ffc505e553d869d3b4be8
bala-amypo <bala@amypo.in>
2025-12-26 14:34:29 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,31 +1,39 @@
 package com.example.demo.security;
 
 import io.jsonwebtoken.*;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.*;
 
 public class JwtUtil {
 
     private String secret;
     private long jwtExpirationMs;
 
-    public String generateToken(String username, String role, Long userId, String email){
-        Map<String,Object> claims = new HashMap<>();
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String username, String role, Long userId, String email) {
+
+        Map<String, Object> claims = new HashMap<>();
         claims.put("role", role);
         claims.put("userId", userId);
         claims.put("email", email);
 
         return Jwts.builder()
-            .setClaims(claims)
-            .setSubject(username)
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-            .signWith(SignatureAlgorithm.HS256, secret)
-            .compact();
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
-    public Jws<Claims> validateAndGetClaims(String token){
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+    public Jws<Claims> validateAndGetClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token);
     }
 }
