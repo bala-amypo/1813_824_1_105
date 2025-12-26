9a28a4732f2c9196a22e9b0514b2754c0ca7ff5c
bala-amypo <bala@amypo.in>
2025-12-26 09:53:44 +0000

    User commit

--- src/main/java/com/example/demo/model/VisitLog.java
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -12,23 +12,14 @@ public class VisitLog {
 
     private LocalDateTime checkInTime;
     private LocalDateTime checkOutTime;
-
-    private Boolean accessGranted = false;
-    private Boolean alertSent = false;
-
-    private String purpose; // <-- ADD THIS FIELD
+    private Boolean accessGranted;
 
     @ManyToOne
-    @JoinColumn(name = "visitor_id")
     private Visitor visitor;
 
     @ManyToOne
-    @JoinColumn(name = "host_id")
     private Host host;
 
-    // ------------------
-    // Getters & Setters
-    // ------------------
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -40,12 +31,6 @@ public class VisitLog {
 
     public Boolean getAccessGranted() { return accessGranted; }
     public void setAccessGranted(Boolean accessGranted) { this.accessGranted = accessGranted; }
-
-    public Boolean getAlertSent() { return alertSent; }
-    public void setAlertSent(Boolean alertSent) { this.alertSent = alertSent; }
-
-    public String getPurpose() { return purpose; }  // <-- Getter
-    public void setPurpose(String purpose) { this.purpose = purpose; } // <-- Setter
 
     public Visitor getVisitor() { return visitor; }
     public void setVisitor(Visitor visitor) { this.visitor = visitor; }
