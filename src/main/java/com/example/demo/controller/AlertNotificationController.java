0e3defbac13d651022eb211480dc94a810612b41
bala-amypo <bala@amypo.in>
2025-12-26 09:03:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertNotificationController.java
+++ src/main/java/com/example/demo/controller/AlertNotificationController.java
@@ -1,34 +1,34 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.model.AlertNotification;
 import com.example.demo.service.AlertNotificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("alerts")
 public class AlertNotificationController {
 
-    private final AlertNotificationService alertService;
+    private AlertNotificationService alertService;
 
     public AlertNotificationController(AlertNotificationService alertService) {
         this.alertService = alertService;
     }
 
-    @PostMapping("/{visitLogId}")
+    @PostMapping("/alerts/{visitLogId}")
     public AlertNotification send(@PathVariable Long visitLogId) {
         return alertService.sendAlert(visitLogId);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/alerts/{id}")
     public AlertNotification get(@PathVariable Long id) {
         return alertService.getAlert(id);
     }
 
-    @GetMapping
+    @GetMapping("/alerts")
     public List<AlertNotification> all() {
         return alertService.getAllAlerts();
     }
