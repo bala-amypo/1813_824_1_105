af99400eeb65ca774dc5985efddf565a7b40c0b3
bala-amypo <bala@amypo.in>
2025-12-26 08:58:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertNotificationRepository.java
+++ src/main/java/com/example/demo/repository/AlertNotificationRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.AlertNotification;
-import java.util.Optional;
-import java.util.List;
 
-public interface AlertNotificationRepository {
-    AlertNotification save(AlertNotification a);
-    Optional<AlertNotification> findById(Long id);
+import com.example.demo.model.AlertNotification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AlertNotificationRepository extends JpaRepository<AlertNotification, Long> {
     Optional<AlertNotification> findByVisitLogId(Long visitLogId);
-    List<AlertNotification> findAll();
 }
