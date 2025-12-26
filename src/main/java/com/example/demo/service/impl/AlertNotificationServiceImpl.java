25d9d80d210050311d37412d57deac25b7b28a9b
bala-amypo <bala@amypo.in>
2025-12-26 09:02:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertNotificationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertNotificationServiceImpl.java
@@ -1,32 +1,32 @@
 package com.example.demo.service.impl;
+
+import com.example.demo.model.AlertNotification;
+import com.example.demo.model.VisitLog;
+import com.example.demo.repository.AlertNotificationRepository;
+import com.example.demo.repository.VisitLogRepository;
+import com.example.demo.service.AlertNotificationService;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.example.demo.model.AlertNotification;
-import com.example.demo.repository.AlertNotificationRepository;
-import com.example.demo.service.AlertNotificationService;
-import com.example.demo.model.VisitLog;
-import com.example.demo.repository.VisitLogRepository;
 
-@Service
-public class AlertNotificationServiceImpl {
+public class AlertNotificationServiceImpl implements AlertNotificationService {
 
     private AlertNotificationRepository alertRepository;
     private VisitLogRepository visitLogRepository;
 
+    @Override
     public AlertNotification sendAlert(Long visitLogId) {
 
         VisitLog log = visitLogRepository.findById(visitLogId)
                 .orElseThrow(() -> new RuntimeException("VisitLog not found"));
 
-        if (alertRepository.findByVisitLogId(visitLogId).isPresent())
+        if (alertRepository.findByVisitLogId(visitLogId).isPresent()) {
             throw new IllegalArgumentException("Alert already sent");
+        }
 
         AlertNotification alert = new AlertNotification();
-        alert.setVisitLog(log);
         alert.setSentTo(log.getHost().getEmail());
-        alert.setAlertMessage("Visitor arrived");
+        alert.setAlertMessage("Visitor " + log.getVisitor().getFullName() + " checked in");
         alert.setSentAt(LocalDateTime.now());
 
         log.setAlertSent(true);
@@ -35,11 +35,13 @@ public class AlertNotificationServiceImpl {
         return alertRepository.save(alert);
     }
 
+    @Override
     public AlertNotification getAlert(Long id) {
         return alertRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Alert not found"));
     }
 
+    @Override
     public List<AlertNotification> getAllAlerts() {
         return alertRepository.findAll();
     }
