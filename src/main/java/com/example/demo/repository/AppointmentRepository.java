b87cdad1f34cbadf3bf076a88ec2ec1b0af38777
bala-amypo <bala@amypo.in>
2025-12-26 08:20:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppointmentRepository.java
+++ src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Appointment;
 import java.util.List;
+import java.util.Optional;
 public interface AppointmentRepository {
     Appointment save(Appointment a);
     Optional<Appointment> findById(Long id);
