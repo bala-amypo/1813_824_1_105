6e5d6cba95c20606cacc48cf30b8071a67489de6
bala-amypo <bala@amypo.in>
2025-12-26 09:01:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/HostServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/HostServiceImpl.java
@@ -1,19 +1,22 @@
 package com.example.demo.service.impl;
+
+import com.example.demo.model.Host;
 import com.example.demo.repository.HostRepository;
 import com.example.demo.service.HostService;
-import com.example.demo.model.Host;
-import java.util.List;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-@Service
-public class HostServiceImpl {
+
+public class HostServiceImpl implements HostService {
 
     private HostRepository hostRepository;
 
-    public Host createHost(Host h) {
-        return hostRepository.save(h);
+    public HostServiceImpl() {
     }
 
+    @Override
+    public Host createHost(Host host) {
+        return hostRepository.save(host);
+    }
+
+    @Override
     public Host getHost(Long id) {
         return hostRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Host not found"));
