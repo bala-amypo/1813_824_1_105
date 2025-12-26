8ff35ed01e1e44da3a5e6937215b2bf41c522b4e
bala-amypo <bala@amypo.in>
2025-12-26 08:57:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitorRepository.java
+++ src/main/java/com/example/demo/repository/VisitorRepository.java
@@ -1,12 +1,7 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Visitor;
-import com.example.demo.repository.VisitorRepository;
-import java.util.Optional;
-import java.util.List;
 
-public interface VisitorRepository {
-    Visitor save(Visitor v);
-    Optional<Visitor> findById(Long id);
-    List<Visitor> findAll();
+import com.example.demo.model.Visitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitorRepository extends JpaRepository<Visitor, Long> {
 }
