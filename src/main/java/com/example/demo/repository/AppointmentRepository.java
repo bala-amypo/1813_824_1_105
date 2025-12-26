1441c237a13a9e14902ea97d5564f6b7b0209767
bala-amypo <bala@amypo.in>
2025-12-26 08:58:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppointmentRepository.java
+++ src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -1,13 +1,11 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-import java.util.Optional;
-public interface AppointmentRepository {
-    Appointment save(Appointment a);
-    Optional<Appointment> findById(Long id);
-    List<Appointment> findAll();
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByHostId(Long hostId);
     List<Appointment> findByVisitorId(Long visitorId);
 }
-
--- src/main/java/com/example/demo/repository/VisitLogRepository.java
+++ src/main/java/com/example/demo/repository/VisitLogRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.VisitLog;
-import com.example.demo.repository.VisitLogRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-import java.util.Optional;
-public interface VisitLogRepository {
-    VisitLog save(VisitLog v);
-    Optional<VisitLog> findById(Long id);
+
+public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
     List<VisitLog> findByCheckOutTimeIsNull();
 }
