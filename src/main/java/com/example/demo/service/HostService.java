891aebd2bd9f7033862508845576519bd2dbcfa4
bala-amypo <bala@amypo.in>
2025-12-26 08:01:37 +0000

    User commit

--- src/main/java/com/example/demo/service/HostService.java
+++ src/main/java/com/example/demo/service/HostService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
+
 import com.example.demo.model.Host;
-import java.util.List;
-public interface HostService{
+
+public interface HostService {
+
     Host createHost(Host host);
+
     Host getHost(Long id);
-    List<Host> getAllHost();
 }
--- src/main/java/com/example/demo/service/VisitLogService.java
+++ src/main/java/com/example/demo/service/VisitLogService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
-import org.springframework.stereotype.Service;
-import com.example.demo.model.VisitLog;
-import java.util.List;
 
-public interface VisitLogService{
-  VisitLog checkInVisitor(Long visitorId,Long hostId,String purpose);
-   VisitLog checkOutVisitor(Long visitLogId);
- List<VisitLog> getActiveVisits();
-  VisitLog getVisitLog(Long id);
+import java.util.List;
+import com.example.demo.model.VisitLog;
+
+public interface VisitLogService {
+
+    VisitLog checkInVisitor(Long visitorId, Long hostId, String purpose);
+
+    VisitLog checkOutVisitor(Long visitLogId);
+
+    VisitLog getVisitLog(Long id);
+
+    List<VisitLog> getActiveVisits();
 }
