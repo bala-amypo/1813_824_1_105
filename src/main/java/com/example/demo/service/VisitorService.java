5d851c89db82cfe0d5b82599a6740bbed9abcf9b
bala-amypo <bala@amypo.in>
2025-12-26 08:00:36 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitorService.java
+++ src/main/java/com/example/demo/service/VisitorService.java
@@ -1,8 +1,13 @@
 package com.example.demo.service;
-import com.example.demo.model.Visitor;
+
 import java.util.List;
-public interface VisitorService{
- Visitor createVisitor(Visitor visitor);
- Visitor getVisitor(Long id);
- List<Visitor> getAllVisitor();
+import com.example.demo.model.Visitor;
+
+public interface VisitorService {
+
+    Visitor createVisitor(Visitor visitor);
+
+    Visitor getVisitor(Long id);
+
+    List<Visitor> getAllVisitors();
 }
