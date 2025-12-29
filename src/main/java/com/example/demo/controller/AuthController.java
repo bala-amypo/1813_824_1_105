50d6f06d37de26d1ee8df26ed7d52dc4670b5428
bala-amypo <bala@amypo.in>
2025-12-29 05:48:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,22 +1,14 @@
 package com.example.demo.controller;
-import com.example.demo.model.User;
-import com.example.demo.security.JwtUtil;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin("*")
 public class AuthController {
-    private JwtUtil jwtUtil = new JwtUtil();
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return user;
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody User user) {
-        return jwtUtil.generateToken(user.getUsername(), user.getRole(), user.getId(), user.getEmail());
+    public String register() {
+        return "User Registered Successfully";
     }
 }
